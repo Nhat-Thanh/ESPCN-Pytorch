4bfba4b50731ad5b5713a415617e0501b1788be5
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 03:52:02 +0700

    add main function and remove redundant code

--- train.py
+++ train.py
@@ -9,10 +9,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--steps",          type=int, default=100000,          help='-')
 parser.add_argument("--scale",          type=int, default=2,               help='-')
 parser.add_argument("--batch-size",     type=int, default=128,             help='-')
-parser.add_argument("--save-every",     type=int, default=100,             help='-')
+parser.add_argument("--save-every",     type=int, default=1000,            help='-')
 parser.add_argument("--save-best-only", type=int, default=0,               help='-')
 parser.add_argument("--ckpt-dir",       type=str, default="checkpoint/x2", help='-')
 
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAG, unparsed = parser.parse_known_args()
 steps = FLAG.steps
@@ -37,7 +41,6 @@ if scale == 3:
 elif scale == 4:
     hr_crop_size = 17 * scale
 
-
 train_set = dataset(dataset_dir, "train")
 train_set.generate(lr_crop_size, hr_crop_size)
 train_set.load_data()
@@ -46,34 +49,23 @@ valid_set = dataset(dataset_dir, "validation")
 valid_set.generate(lr_crop_size, hr_crop_size)
 valid_set.load_data()
 
-test_set = dataset(dataset_dir, "test")
-test_set.generate(lr_crop_size, hr_crop_size)
-test_set.load_data()
-
 
 # -----------------------------------------------------------
 #  Train
 # -----------------------------------------------------------
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-fsrcnn = ESPCN(scale, device)
-fsrcnn.setup(optimizer=torch.optim.Adam(fsrcnn.model.parameters(), lr=2e-4),
-            loss=torch.nn.MSELoss(),
-            model_path=model_path,
-            ckpt_path=ckpt_path,
-            metric=PSNR)
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    espcn = ESPCN(scale, device)
+    espcn.setup(optimizer=torch.optim.Adam(espcn.model.parameters(), lr=2e-4),
+                loss=torch.nn.MSELoss(), model_path=model_path,
+                ckpt_path=ckpt_path, metric=PSNR)
 
-fsrcnn.load_checkpoint(ckpt_path)
-fsrcnn.train(train_set, valid_set, 
-            steps=steps, batch_size=batch_size,
-            save_best_only=save_best_only, 
-            save_every=save_every)
+    espcn.load_checkpoint(ckpt_path)
+    espcn.train(train_set, valid_set, 
+                steps=steps, batch_size=batch_size,
+                save_best_only=save_best_only, 
+                save_every=save_every)
 
-
-# -----------------------------------------------------------
-#  Test
-# -----------------------------------------------------------
-# todo: use the best checkpoint to evaluate the test set
-fsrcnn.load_weights(model_path)
-loss, metric = fsrcnn.evaluate(test_set)
-print(f"loss: {loss} - psnr: {metric}")
+if __name__ == "__main__":
+    main()
